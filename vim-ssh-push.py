f3d638523b280e6a15b9669d408fc1046c57df59
Emil Bryggare <emil@emilbryggare.com>
2012-11-26 19:24:09 +0100

    Some more work completed. Added automatic support for symlinks

--- vim-ssh-push.py
+++ vim-ssh-push.py
@@ -2,44 +2,99 @@
 import os
 
 # Configuration
-servers = ["emilbryggare.se"]
-files = [".vim", "vim.fork", ".vimrc", ".vimrc.fork", ".vimrc.bundles", ".vimrc.bundles.fork"]
-local_home_dir = "/Users/emil"
+servers = ["dev32", "emilbryggare.se"]
+files = [".spf13-vim-3",
+         ".vim",
+         ".vimrc",
+         ".vimrc.bundles",
+         ".vimrc.local"]
 
+local_home_directory = os.path.expanduser('~')
+os.chdir(local_home_directory)
+symlinks = {}
 # Clear users view
 os.system("clear")
 
-# Get remote home dir
-remote_home_dir = "/home/emil"
+def validate_files(files):
+    print "Validating files..."
+    for file in files:
+        if (os.path.exists(file) == False):
+            print "-> Not a valid file " + file
+            files.remove(file)
 
-def ssh_remove_old_files(files, server):
-    remove_command = "ssh " + str(server) + " 'rm -rf "
+
+def find_symlinks(files, symlinks):
+    print "Finding symlinks..."
+    for file in files:
+        symlink =  os.popen("find ~ -name '" + file + "' -maxdepth 1 -type l -ls | awk -F \"-> \" '{ print $2}'").read().replace("\n","")
+        if (symlink):
+            print "-> Found " + file
+            symlinks[file] = symlink.replace(local_home_directory, "~")
+
+    # Remove files that are symlinks as we create them in the create_symlinks
+    # function.
+    for file in symlinks:
+        files.remove(file)
+
+def cleanup(files, symlinks, server):
+    print "Removing old files and symlinks..."
+    cmd = "ssh " + str(server) + " 'rm -rf "
 
     for file in files:
         if (file == files[-1]):
-            remove_command += str(file) + "'"
+            cmd += str(file) + "'"
         else:
-            remove_command += str(file) + " "
+            cmd += str(file) + " "
+    symlinks_iter = 0
 
-    os.system(remove_command)
+    for file in symlinks:
+        if (len(symlinks) == symlinks_iter):
+            cmd += str(file) + "'"
+        else:
+            cmd += str(file) + " "
 
-def ssh_symlink_files(files, server):
-    symlink_command = "ssh " + str(server) + " '"
 
+    os.system(cmd)
+
+def create_symlinks(symlinks, server):
+    print "Creating symlinks..."
+    cmd = "ssh " + str(server) + " '"
+
+    symlinks_iter = 0
+    for file in symlinks:
+        symlinks_iter = symlinks_iter + 1
+        print symlinks[file]
+        cmd += "ln -f -s " + symlinks[file] + " " + file
+        if (len(symlinks) == symlinks_iter):
+            cmd += "'"
+        else:
+            cmd += " && "
+
+    os.system(cmd)
+
+def transfer_archived_files(server):
+    print "Transfering archived files..."
+    cmd = "scp ssh-push.tar "
+    cmd += server + ":"
+    os.system(cmd)
+
+def archive_files(files):
+    print "Archiving files..."
+    cmd = "tar --check-links -cvf ssh-push.tar "
     for file in files:
-        symlink_command += "ln -s " + str(remote_home_dir) + "/.spf13-vim-3/" + str(file) + " " + str(file)
-        if (file == files[-1]):
-            symlink_command += "'"
-        else:
-            symlink_command += " && "
+        cmd += file + " "
+    os.system(cmd)
 
-    os.system(symlink_command)
+def extract_files(server):
+    print "Extracting files..."
+    cmd = "ssh " + server + " 'tar xvf ssh-push.tar && rm ssh-push.tar'"
+    os.system(cmd)
 
-os.chdir(local_home_dir)
-
+validate_files(files)
+find_symlinks(files, symlinks)
+archive_files(files)
 for server in servers:
-    os.system("tar --check-links -cvf  vim-ssh-push.tar .spf13-vim-3/ && scp vim-ssh-push.tar " + server + ": && ssh "+ server + " 'tar xvf vim-ssh-push.tar'")
-    ssh_remove_old_files(files, server)
-    ssh_symlink_files(files, server)
-
-print "Done!"
+    cleanup(files, symlinks, server)
+    transfer_archived_files(server)
+    extract_files(server)
+    create_symlinks(symlinks, server)
